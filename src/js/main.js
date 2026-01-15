dc1ee3fc11b4bc2d3205afb31e4e08f6d1234968
Kerolos-Morcos <122631134+Kerolos-Morcos@users.noreply.github.com>
2026-01-15 19:00:56 +0200

    Loading

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
+import Loader from "./ui/loadingSpinner.js";
+
 /**
  * NutriPlan - Main Entry Point
- * 
+ *
  * This is the main entry point for the application.
  * Import your modules and initialize the app here.
  */
 
-
+// Loading..
+Loader.loading();
---
+++ src/js/ui/loadingSpinner.js
@@ -0,0 +1,25 @@
+class Loader {
+  static loading() {
+    const loader = document.getElementById("app-loading-overlay");
+    if (!loader) return;
+    loader.style.cssText = `
+      position: fixed;
+      inset: 0;
+      z-index: 999999999;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: white;
+      transition: opacity 0.7s;
+    `;
+
+    window.addEventListener("load", () => {
+      loader.style.opacity = "0";
+      setTimeout(() => {
+        loader.style.display = "none";
+      }, 500);
+    });
+  }
+}
+
+export default Loader;
